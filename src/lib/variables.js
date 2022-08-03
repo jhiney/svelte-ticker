8f960823cc1b6280e46db39905b5b652c32f9bac
jhiney <hiney.jake@outlook.com>
2022-08-03 09:41:00 -0500

    base URL to variables

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,4 +1,5 @@
 export const variables = {
-    baseKey: import.meta.env.VITE_ALPACA_KEY,
-    secretKey: import.meta.env.VITE_ALPACA_SECRET,
-  };
+	baseKey: import.meta.env.VITE_ALPACA_KEY,
+	secretKey: import.meta.env.VITE_ALPACA_SECRET,
+	ALPACA_BASE_URL: "https://paper-api.alpaca.markets/"
+};
