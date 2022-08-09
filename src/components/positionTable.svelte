bd235b1b23fd43d0d7273acf53fc8d42edc71d33
jhiney <hiney.jake@outlook.com>
2022-08-09 09:56:05 -0500

    remove uneeded import

--- src/components/positionTable.svelte
+++ src/components/positionTable.svelte
@@ -3,7 +3,6 @@
 	import { positions } from "../lib/store.js";
 	import getPositions from "../lib/getPositions.js"
 	import TestCard from "./testCard.svelte";
-import Header from "./Header.svelte";
 
 	onMount(async () => {
 		getPositions();	
