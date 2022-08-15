199256543b3086ff802a455cb20a91538957612e
jhiney <hiney.jake@outlook.com>
2022-08-15 11:59:22 -0500

    testing skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 const config = {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: [
+		"./src/**/*.{html,js,svelte,ts}",
+		"./node_modules/@brainandbones/skeleton/**/*.{html,js,svelte,ts}"
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		require('@brainandbones/skeleton/tailwind.cjs')
+	]
 };
 
 module.exports = config;
