b6569393a944c8a6bc7d1b0bd1e11456d37891c6
jhiney <hiney.jake@outlook.com>
2022-08-16 15:50:41 -0500

    Make symbol a button for later use

---
+++ src/components/CompanyLogo.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let symbol;
+	import { onMount } from "svelte";
+	import { Button } from "@brainandbones/skeleton";
+
+	onMount(async () => {
+		//getPositions();
+	});
+</script>
+
+<div class="font-bold">
+	<Button
+		size="base"
+		background="bg-primary-500"
+		color="text-white"
+		ring="ring-transparent"
+		weight="ring-none"
+		rounded="rounded-full"
+		width="w-auto"
+		disabled={false}
+	>
+		{symbol}
+	</Button>
+</div>
